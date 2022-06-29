eceb05b9ce1bbbe49093c31bfa5a096af63e9e1f
BlueStaxks <108381028+BlueStaxks@users.noreply.github.com>
2022-06-29 21:21:59 +0900

    upload

---
+++ AJOU code9-1.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+    int m[5] = {0};
+    int i, max, min;
+    printf("Enter five integers separated by blanks.\n");
+    for (i = 0; i < 5; ++i)
+    {
+        scanf("%d", &m[i]);
+        if (i == 0)
+            max = min = m[0];
+        max < m[i] ? max = m[i] : 0;
+        min > m[i] ? min = m[i] : 0;
+    }
+    printf("min is %d. max is %d.", min, max);
+    return 0;
+}
