fd04a0767e2ec44d177fd9bf9ae6d777f3496d19
BlueStaxks <108381028+BlueStaxks@users.noreply.github.com>
2022-06-29 20:21:20 +0900

    upload

--- AJOU code1.cpp
+++ AJOU code1.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 int main()
 {
 	int a = 0b110010011000111100;
---
+++ AJOU code2.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a = 0xB1A2EC9;
+	int i, p;
+	for (i = 31; i >= 0; i--)
+	{
+		p = a >> i & 1;
+		printf("%d", p);
+	}
+	return 0;
+}
